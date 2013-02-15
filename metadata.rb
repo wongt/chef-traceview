4a8edbc73f2da4ab78924340c60b66aa8f5e5bc1
James Meickle <jmeickle@appneta.com>
2013-02-15 09:47:43 -0500

    Remove yum and apt dependencies and increment version number.

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,4 @@ maintainer_email "YOUR_EMAIL"
 license          "All rights reserved"
 description      "Installs/Configures tracelytics"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.0.2"
-
-depends "apt"
-depends "yum"
+version          "0.0.3"
