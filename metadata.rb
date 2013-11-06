5a8b066f1fb136a627ae5194fe3e22cde8f737b8
James Meickle <jmeickle@appneta.com>
2013-11-06 13:46:30 -0500

    Add a package maintainer.

--- metadata.rb
+++ metadata.rb
@@ -1,6 +1,6 @@
-maintainer       "YOUR_COMPANY_NAME"
-maintainer_email "YOUR_EMAIL"
+maintainer       "AppNeta"
+maintainer_email "jmeickle@appneta.com"
 license          "All rights reserved"
-description      "Installs/Configures tracelytics"
+description      "Installs and configures AppNeta TraceView."
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.0.7"
