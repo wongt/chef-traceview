b18ceb4baa689584e3df303601fcbca6e1accc99
wongt <wong.teddy@gmail.com>
2014-04-16 15:20:27 -0400

    Added yum community cookbook version 3.1.2 dependecy

--- metadata.rb
+++ metadata.rb
@@ -3,4 +3,9 @@ maintainer_email "jmeickle@appneta.com"
 license          "All rights reserved"
 description      "Installs and configures AppNeta TraceView."
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.0.8"
+version          "0.0.9"
+
+depends 'yum', '~> 3.1.2'
+
+#v0.0.9		Teddy Wong <wong.teddy@gmail.com> 
+#		Updated version of chef-tracview to use yum community cookbook version 3.1.2
